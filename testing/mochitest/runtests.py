cd6b031e4b2209993028fdb2fc9d30dad639b1f5
Stephen Pohl <spohl.mozilla.bugs@gmail.com>
2014-09-18 16:23:03 -0400

    Bug 1060652: Make mochitest harness work with the new bundle structure for v2 signatures on OSX. r=ted

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -307,6 +307,7 @@ class MochitestServer(object):
     # get testing environment
     env = environment(xrePath=self._xrePath)
     env["XPCOM_DEBUG_BREAK"] = "warn"
+    env["DYLD_LIBRARY_PATH"] = os.path.join(os.path.dirname(self._xrePath), 'MacOS')
     env["LD_LIBRARY_PATH"] = self._xrePath
 
     # When running with an ASan build, our xpcshell server will also be ASan-enabled,
@@ -689,6 +690,8 @@ class MochitestUtilsMixin(object):
     # specified, try to select the one from hostutils.zip, as required in bug 882932.
     if not options.httpdPath:
       options.httpdPath = os.path.join(options.utilityPath, "components")
+      if not os.path.exists(options.httpdPath) and (sys.platform == 'osx' or sys.platform == "darwin"):
+        options.httpdPath = os.path.join(os.path.dirname(options.utilityPath), "Resources", "components")
 
     self.startWebServer(options)
     self.startWebSocketServer(options, debuggerInfo)
@@ -887,6 +890,7 @@ class SSLTunnel:
       exit(1)
 
     env = environment(xrePath=self.xrePath)
+    env["DYLD_LIBRARY_PATH"] = os.path.join(os.path.dirname(self.xrePath), 'MacOS')
     env["LD_LIBRARY_PATH"] = self.xrePath
     self.process = mozprocess.ProcessHandler([ssltunnel, self.configFile],
                                                env=env)
@@ -1065,6 +1069,7 @@ class Mochitest(MochitestUtilsMixin):
 
     # Pre-create the certification database for the profile
     env = self.environment(xrePath=options.xrePath)
+    env["DYLD_LIBRARY_PATH"] = os.path.join(os.path.dirname(options.xrePath), 'MacOS')
     env["LD_LIBRARY_PATH"] = options.xrePath
     bin_suffix = mozinfo.info.get('bin_suffix', '')
     certutil = os.path.join(options.utilityPath, "certutil" + bin_suffix)
