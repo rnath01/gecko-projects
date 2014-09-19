d68c8c6bc1d2214e3fedb03eb359eee30b6ce4cb
Chris Manchester <cmanchester@mozilla.com>
2014-09-18 23:46:51 -0400

    A Marionette test failure in python and javascript.

--- testing/marionette/client/marionette/tests/unit/test_simpletest_pass.js
+++ testing/marionette/client/marionette/tests/unit/test_simpletest_pass.js
@@ -7,6 +7,7 @@ MARIONETTE_TIMEOUT = 1000;
 is(2, 2, "test for is()");
 isnot(2, 3, "test for isnot()");
 ok(2 == 2, "test for ok()");
+is(1, 2, "it is or it isn't, isn't it");
 
 setTimeout(finish, 100);
 
