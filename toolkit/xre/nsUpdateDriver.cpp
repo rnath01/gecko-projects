5500bb5a093af4e7e50e975ea188f735cb01eef4
Stephen Pohl <spohl.mozilla.bugs@gmail.com>
2014-09-19 15:53:25 -0700

    Bug 1046924: Move updates directory out of the .app bundle. r=rstrong

--- toolkit/xre/nsUpdateDriver.cpp
+++ toolkit/xre/nsUpdateDriver.cpp
@@ -379,7 +379,7 @@ CopyUpdaterIntoUpdateDir(nsIFile *greDir, nsIFile *appDir, nsIFile *updateDir,
 {
   // Copy the updater application from the GRE and the updater ini from the app
 #if defined(XP_MACOSX)
-  if (!CopyFileIntoUpdateDir(greDir, kUpdaterApp, updateDir))
+  if (!CopyFileIntoUpdateDir(appDir, kUpdaterApp, updateDir))
     return false;
   CopyFileIntoUpdateDir(greDir, kUpdaterINI, updateDir);
 #else
