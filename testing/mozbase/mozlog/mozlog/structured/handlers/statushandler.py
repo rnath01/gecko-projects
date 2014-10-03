b0cec7aefd118aefb49888316c4c9a1447f2d0c0
Chris Manchester <cmanchester@mozilla.com>
2014-10-03 10:40:19 -0400

    Backed out changeset 260a1b3ded35 (bug 1066785)

--- testing/mozbase/mozlog/mozlog/structured/handlers/statushandler.py
+++ testing/mozbase/mozlog/mozlog/structured/handlers/statushandler.py
@@ -10,20 +10,17 @@ from collections import (
 from mozlog.structured.structuredlog import log_levels
 
 RunSummary = namedtuple("RunSummary",
-                        ("unexpected_statuses",
-                         "expected_statuses",
-                         "log_level_counts",
-                         "action_counts"))
+                        ("unexpected", "skipped", "log_level_counts", "action_counts"))
 
 class StatusHandler(object):
     """A handler used to determine an overall status for a test run according
     to a sequence of log messages."""
 
     def __init__(self):
-        # The count of each type of unexpected result status (includes tests and subtests)
-        self.unexpected_statuses = defaultdict(int)
-        # The count of each type of expected result status (includes tests and subtests)
-        self.expected_statuses = defaultdict(int)
+        # The count of unexpected results (includes tests and subtests)
+        self.unexpected = 0
+        # The count of skip results (includes tests and subtests)
+        self.skipped = 0
         # The count of actions logged
         self.action_counts = defaultdict(int)
         # The count of messages logged at each log level
@@ -38,17 +35,16 @@ class StatusHandler(object):
             self.log_level_counts[data['level']] += 1
 
         if action in ('test_status', 'test_end'):
-            status = data['status']
             if 'expected' in data:
-                self.unexpected_statuses[status] += 1
-            else:
-                self.expected_statuses[status] += 1
+                self.unexpected += 1
 
+            if data['status'] == 'SKIP':
+                self.skipped += 1
 
     def summarize(self):
         return RunSummary(
-            dict(self.unexpected_statuses),
-            dict(self.expected_statuses),
+            self.unexpected,
+            self.skipped,
             dict(self.log_level_counts),
             dict(self.action_counts),
         )
